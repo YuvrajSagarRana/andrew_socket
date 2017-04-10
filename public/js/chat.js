51665be6061396ed7bc464c8345546f5af5e9564
Sagar Rana <Sagar Rana>
2017-04-10 15:48:46 -0700

    Finally production

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,64 +1,72 @@
 var socket = io();
-function scrollToDown(){
-  var message=jQuery('#messages');
-  var lastMessage=message.children('li:last-child');
-    var clientHeight=message.prop('clientHeight');
-    var scrollTop=message.prop('scrollTop');
-    var scrollHeight=message.prop('scrollHeight');
-    var lastMessageHeight=lastMessage.innerHeight();
-    var secondMessageHeight=lastMessage.prev().innerHeight();
-    if(clientHeight+scrollTop+secondMessageHeight+lastMessageHeight>=scrollHeight){
-      message.scrollTop(scrollHeight);
-    }
+
+function scrollToBottom () {
+  // Selectors
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child')
+  // Heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
 }
 
 socket.on('connect', function () {
-  var params=jQuery.deparam(window.location.search);
-  socket.emit('join',params,function(err){
-    if(err){
-    alert(err);
-    window.location.href='/';
-    }else{
-     console.log('No error')
-    }
+  var params = jQuery.deparam(window.location.search);
 
-  })
+  socket.emit('join', params, function (err) {
+    if (err) {
+      alert(err);
+      window.location.href = '/';
+    } else {
+      console.log('No error');
+    }
+  });
 });
 
 socket.on('disconnect', function () {
   console.log('Disconnected from server');
 });
-socket.on('userUpdateList',function(users){
-  console.log('userlist',users)
-})
-socket.on('newMessage', function (message) {
-  var formattedTime=moment(message.createdAt).format('h:mm a')
-  var template=jQuery('#message-template').html();
-  var html=Mustache.render(template,{
-    text:message.text,
-      from:message.from,
-      create:formattedTime,
-  });
-    jQuery('#messages').append(html);
-    scrollToDown();
- /*
-  console.log('newMessage', message);
-  var li = jQuery('<li></li>');
-  li.text(`${message.from} ${formattedTime}: ${message.text}`);
 
-  jQuery('#messages').append(li);*/
+socket.on('updateUserList', function (users) {
+  var ol = jQuery('<ol></ol>');
+
+  users.forEach(function (user) {
+    ol.append(jQuery('<li></li>').text(user));
+  });
+
+  jQuery('#users').html(ol);
+});
+
+socket.on('newMessage', function (message) {
+  var formattedTime = moment(message.createdAt).format('h:mm a');
+  var template = jQuery('#message-template').html();
+  var html = Mustache.render(template, {
+    text: message.text,
+    from: message.from,
+    createdAt: formattedTime
+  });
+
+  jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 socket.on('newLocationMessage', function (message) {
-    var formattedTime=moment(message.createdAt).format('h:mm a')
-    var template=jQuery('#location-message-template').html();
-    var html=Mustache.render(template,{
-        url:message.url,
-        from:message.from,
-        create:formattedTime,
-    });
-    jQuery('#messages').append(html);
-    scrollToDown();
+  var formattedTime = moment(message.createdAt).format('h:mm a');
+  var template = jQuery('#location-message-template').html();
+  var html = Mustache.render(template, {
+    from: message.from,
+    url: message.url,
+    createdAt: formattedTime
+  });
+
+  jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function (e) {
@@ -67,7 +75,6 @@ jQuery('#message-form').on('submit', function (e) {
   var messageTextbox = jQuery('[name=message]');
 
   socket.emit('createMessage', {
-    from: 'User',
     text: messageTextbox.val()
   }, function () {
     messageTextbox.val('')
